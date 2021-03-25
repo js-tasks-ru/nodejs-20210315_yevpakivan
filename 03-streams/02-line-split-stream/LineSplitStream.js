7bf04bfd2aaa5190f0ee49918d6531c0d8097cc2
ivan_yevpak <ivan.yevpak@newfireglobal.com>
2021-03-25 19:27:47 +0200

    add solution for 2-module 2-task

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,28 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.line = '';
+    this.lastEl = '';
   }
 
   _transform(chunk, encoding, callback) {
+    this.line = `${this.lastEl}${chunk.toString()}`;
+
+    if (this.line.includes(os.EOL)) {
+      const [line, ...rest] = this.line.split(os.EOL);
+      this.push(line);
+      this.lastEl = rest.join(os.EOL);
+    } else {
+      this.lastEl = this.line;
+    }
+
+    callback();
   }
 
   _flush(callback) {
+    this.lastEl.split(os.EOL).forEach((item) => this.push(item));
+
+    callback();
   }
 }
 
